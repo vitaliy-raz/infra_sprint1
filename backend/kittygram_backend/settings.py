326713c1d05aff378120a9729d6b9efb694ffcef
Vitaliy Razlivanov <vitaliy_raz@mail.ru>
2023-08-03 16:43:58 +0000

    Fix project

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = 'true' == os.getenv('DEBUG').lower()
 
-ALLOWED_HOSTS = ['158.160.77.25', '127.0.0.1', 'localhost', 'kittygram-vit.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
